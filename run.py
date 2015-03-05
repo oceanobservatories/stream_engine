78904be9a078e42248124552f24a6c1e009e497f
Peter Cable <petercable@gmail.com>
2015-03-05 14:27:59 -0800

    fix commented out standalone execute

--- run.py
+++ run.py
@@ -1,28 +1,28 @@
-# from engine import main
-# main.app.run(debug=True)
+from engine import routes
+routes.app.run(debug=True)
 
 # from gevent.wsgi import WSGIServer
-# from engine import main
+# from engine import routes
 #
-# http_server = WSGIServer(('', 5000), main.app)
+# http_server = WSGIServer(('', 5000), routes.app)
 # http_server.serve_forever()
 
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-import tornado.log
-from engine import routes
-import logging
-
-
-def main():
-
-    tornado.log.enable_pretty_logging()
-    server = HTTPServer(WSGIContainer(routes.app))
-    server.bind(5000)
-    server.start(0)
-    logging.getLogger('tornado.application').setLevel(logging.DEBUG)
-    IOLoop.current().start()
-
-if __name__ == '__main__':
-    main()
+# from tornado.wsgi import WSGIContainer
+# from tornado.httpserver import HTTPServer
+# from tornado.ioloop import IOLoop
+# import tornado.log
+# from engine import routes
+# import logging
+#
+#
+# def main():
+#
+#     tornado.log.enable_pretty_logging()
+#     server = HTTPServer(WSGIContainer(routes.app))
+#     server.bind(5000)
+#     server.start(0)
+#     logging.getLogger('tornado.application').setLevel(logging.DEBUG)
+#     IOLoop.current().start()
+#
+# if __name__ == '__main__':
+#     main()
