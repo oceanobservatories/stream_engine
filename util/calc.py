8b8708fd0fb5e36b085c1bb32958c7d41ee430fe
Benjamin Doan <Benjamin.Doan@raytheon.com>
2015-04-30 14:28:05 -0500

    Issue #3351 added the provenance key to stream engine output

--- util/calc.py
+++ util/calc.py
@@ -300,6 +300,7 @@ class DataStream(object):
             fields = self.row_cache[0]._fields
             array = numpy.array(self.row_cache)
 
+
             # Start - Special case to forward Deployment Number
             index = fields.index('deployment')
             data_slice = array[:, index]
@@ -309,6 +310,15 @@ class DataStream(object):
                 'source': source
             }
             # Stop - Special case to forward Deployment Number
+
+            prov_index = fields.index('provenance')
+            prov_data_slice = array[:, prov_index]
+            prov_data_slice = numpy.array(prov_data_slice.tolist())
+            self.data_cache['provenance'] = {
+                'data': prov_data_slice,
+                'source': source
+            }
+
 
             for p in parameters:
                 index = fields.index(p.name.lower())
@@ -546,6 +556,9 @@ class Particle_Generator(object):
             particle['pk'] = pk
             pk['time'] = t
             pk['deployment'] = chunk['deployment']['data'][index]
+
+            # the uuid provenance key needs to be converted to a string
+            particle['provenance'] = str(chunk['provenance']['data'][index])
             for param in parameters:
                 if param.id in chunk:
                     value = chunk[param.id]['data'][index]
