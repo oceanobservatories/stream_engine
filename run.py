4de7eecefd103769207c14d4a36e39e7d7b6c234
Stephen Zakrewsky <Stephen.Zakrewsky@raytheon.com>
2015-04-30 20:15:40 +0000

    REVIEW ONLY: initial test with logging, and other little things
    Change-Id: I7a46403ca5cb3268e1114c72ac0da95619b0d85b

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
+import logging
+root = logging.getLogger()
+root.setLevel('DEBUG')
+root.addHandler(logging.StreamHandler())
+
 from engine.routes import app
 app.run(debug=True)
