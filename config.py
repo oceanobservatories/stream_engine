cbd8720a3e49015276d3e04e6b68ce815017a2f3
Johnathon A Rusk <Johnathon.A.Rusk@Raytheon.com>
2015-05-27 18:36:05 -0400

    Issue #3383 - Utilize the new ‘preload-database’ project
    Note: The ‘preload-database’ project was added as ‘preload_database’ to resolve python import issues.
    
    Change-Id: I1241d88c255f08f5398ea2252e50ed78430165c4

--- config.py
+++ config.py
@@ -1,20 +1,9 @@
-import os
-
-_basedir = os.path.abspath(os.path.dirname(__file__))
-
 DEBUG = False
-
-DBFILE_LOCATION = os.path.join(_basedir, 'preload.db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DBFILE_LOCATION
-DATABASE_CONNECT_OPTIONS = {}
 
 CASSANDRA_CONTACT_POINTS = ['127.0.0.1']
 CASSANDRA_KEYSPACE = 'ooi'
 CASSANDRA_CONNECT_TIMEOUT = 60
 CASSANDRA_FETCH_SIZE = 1000
-
-SPREADSHEET_KEY = '1jIiBKpVRBMU5Hb1DJqyR16XCkiX-CuTsn1Z1VnlRV4I'
-USE_CACHED_SPREADSHEET = False
 
 POOL_SIZE = 4
 
