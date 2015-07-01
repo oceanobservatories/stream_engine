edd29a1377991148d08ff9525019da7ae6b49c07
Stephen Zakrewsky <Stephen.Zakrewsky@raytheon.com>
2015-07-01 20:17:08 +0000

    Future to batch run execute_concurrent with process pools, various tuning parameters

--- profile.py
+++ profile.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 from werkzeug.contrib.profiler import ProfilerMiddleware
+from util.cass import get_session, create_execution_pool
+import preload_database.database
+preload_database.database.initialize_connection(preload_database.database.PreloadDatabaseMode.POPULATED_FILE)
+preload_database.database.open_connection()
+get_session()
+create_execution_pool()
 
 from engine.routes import app
 app.config['PROFILE'] = True
