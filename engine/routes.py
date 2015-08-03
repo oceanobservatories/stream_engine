c4040b94ea3af7e12cec17334d241ead47dccd0f
Adam Taylor <Adam.K.Taylor@raytheon.com>
2015-08-03 12:28:54 -0500

    Issue #6417 Include request UUID and other parameters in Provenace

--- engine/routes.py
+++ engine/routes.py
@@ -75,7 +75,8 @@ def particles():
     annotate = input_data.get('include_annotations', False)
     resp = Response(util.calc.get_particles(input_data.get('streams'), start, stop, input_data.get('coefficients', {}),
                     input_data.get('qcParameters', {}), limit=limit, custom_times=input_data.get('custom_times'),
-                    custom_type=input_data.get('custom_type'), include_provenance=prov, include_annotations=annotate ,strict_range=input_data.get('strict_range', False)),
+                    custom_type=input_data.get('custom_type'), include_provenance=prov, include_annotations=annotate ,
+                    strict_range=input_data.get('strict_range', False), request_uuid=input_data.get('requestUUID','')),
                 mimetype='application/json')
 
     log.info("Request took {:.2f}s to complete".format(time.time() - request_start_time))
@@ -167,7 +168,7 @@ def netcdf():
     resp = Response(util.calc.get_netcdf(input_data.get('streams'), start, stop, input_data.get('coefficients', {}),
                                          limit=limit, custom_times=input_data.get('custom_times'),
                                          custom_type=input_data.get('custom_type'), include_provenance=prov,
-                                         include_annotations=annotate),
+                                         include_annotations=annotate, request_uuid=input_data.get('requestUUID', '')),
                     mimetype='application/netcdf')
 
     log.info("Request took {:.2f}s to complete".format(time.time() - request_start_time))
