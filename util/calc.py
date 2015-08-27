5b2eed0d530ba3edec40b3d306d0b2ea64e0db79
Stephen Zakrewsky <Stephen.Zakrewsky@raytheon.com>
2015-08-27 20:24:39 +0000

    Pull out json into separate module, with some more tests
    Change-Id: I11883e351cf4fa97eb16dd4f16bd2f77c0437f3e

--- util/calc.py
+++ util/calc.py
@@ -14,7 +14,7 @@ import scipy as sp
 import requests
 
 from util.cass import get_streams, get_distinct_sensors, fetch_nth_data, fetch_all_data, fetch_annotations, \
-    get_available_time_range, fetch_l0_provenance, time_to_bin, store_qc_results, bin_to_time, get_location_metadata, \
+    get_available_time_range, fetch_l0_provenance, time_to_bin, bin_to_time, get_location_metadata, \
     get_san_location_metadata, get_full_cass_dataset, insert_dataset
 from util.common import log_timing, ntp_to_datestring,ntp_to_ISO_date, StreamKey, TimeRange, CachedParameter, \
     FUNCTION, CoefficientUnavailableException, UnknownFunctionTypeException, \
@@ -25,6 +25,7 @@ from parameter_util import PDRef
 from engine.routes import  app
 
 import datamodel
+from jsonresponse import JsonResponse
 import xray_interpolation as xinterp
 from datetime import datetime
 import time
@@ -95,111 +96,12 @@ def get_particles(streams, start, stop, coefficients, qc_parameters, limit=None,
     provenance_metadata.add_query_metadata(stream_request, request_uuid, 'JSON')
     pd_data = fetch_pd_data(stream_request, streams, start, stop, coefficients, limit, provenance_metadata, annotation_store)
 
-    # convert data into a list of particles
-    primary_key = stream_keys[0]
-    particles = []
-
-    time_param = primary_key.stream.time_parameter
-    if time_param not in pd_data:
-        raise MissingTimeException("Time param: {} is missing from the primary stream".format(time_param))
-
-    virtual_id_sub = None
-    for index in range(len(pd_data[time_param][primary_key.as_refdes()]['data'])):
-        particle = OrderedDict()
-        particle_id = None
-        particle_bin = None
-
-        if not primary_key.stream.is_virtual:
-            particle['pk'] = primary_key.as_dict()
-
-            # Add non-param data to particle
-            particle['pk']['deployment'] = pd_data['deployment'][primary_key.as_refdes()]['data'][index]
-            particle['pk']['time'] = pd_data[primary_key.stream.time_parameter][primary_key.as_refdes()]['data'][index]
-            particle['provenance'] = str(pd_data['provenance'][primary_key.as_refdes()]['data'][index])
-            particle_id = pd_data['id'][primary_key.as_refdes()]['data'][index]
-            particle_bin = pd_data['bin'][primary_key.as_refdes()]['data'][index]
-        else:
-            if 'id' in pd_data:
-                if virtual_id_sub is None:
-                    for key in pd_data['id']:
-                        if len(pd_data['id'][key]['data']) == len(pd_data[time_param][primary_key.as_refdes()]['data']):
-                            virtual_id_sub = key
-                            break
-                particle_id = pd_data['id'][virtual_id_sub]['data'][index]
-
-            if 'bin' in pd_data:
-                if virtual_id_sub is None:
-                    for key in pd_data['bin']:
-                        if len(pd_data['bin'][key]['data']) == len(pd_data[time_param][primary_key.as_refdes()]['data']):
-                            virtual_id_sub = key
-                            break
-                particle_bin = pd_data['bin'][virtual_id_sub]['data'][index]
-
-        for param in stream_request.parameters:
-            if param.id in pd_data:
-                val = None
-                try:
-                    val = pd_data[param.id][primary_key.as_refdes()]['data'][index]
-                except Exception as e:
-                    log.info("Failed to get data for {}: {}".format(param.id, e))
-                    continue
-
-                if isinstance(val, numpy.ndarray):
-                    val = val.tolist()
-
-                particle[param.name] = val
-
-            # add qc results to particle
-            for qc_function_name in qc_stream_parameters.get(param.name, []):
-                qc_function_results = '%s_%s' % (param.name, qc_function_name)
-
-                if qc_function_results in pd_data\
-                        and pd_data.get(qc_function_results, {}).get(primary_key.as_refdes(), {}).get('data') is not None:
-                    value = pd_data[qc_function_results][primary_key.as_refdes()]['data'][index]
-
-                    qc_results_key = '%s_%s' % (param.name, 'qc_results')
-                    qc_ran_key = '%s_%s' % (param.name, 'qc_executed')
-                    if qc_results_key not in particle:
-                        particle[qc_results_key] = 0b0000000000000000
-
-                    qc_results_value = particle[qc_results_key]
-                    qc_cached_function = CachedFunction.from_qc_function(qc_function_name)
-                    qc_results_mask = int(qc_cached_function.qc_flag, 2)
-
-                    particle[qc_ran_key] = qc_results_mask | particle.get(qc_ran_key, qc_results_mask)
-
-                    if value == 0:
-                        qc_results_value = ~qc_results_mask & qc_results_value
-                    elif value == 1:
-                        qc_results_value = qc_results_mask ^ qc_results_value
-
-                    particle[qc_results_key] = qc_results_value
-
-                    if particle_id is not None and particle_bin is not None:
-                        if not primary_key.stream.is_virtual:
-                            store_qc_results(qc_results_value, particle.get('pk'), particle_id, particle_bin, param.name)
-                        else:
-                            if virtual_id_sub is not None:
-                                sub_pk = primary_key.as_dict()
-                                sub_pk['deployment'] = pd_data['deployment'][virtual_id_sub]['data'][index]
-                                store_qc_results(qc_results_value, sub_pk, particle_id, particle_bin, param.name)
-
-        particles.append(particle)
-
-    if include_provenance or include_annotations:
-        out = OrderedDict()
-        out['data'] = particles
-        if include_provenance:
-            out['provenance'] = provenance_metadata.get_provenance_dict()
-            out['computed_provenance'] = provenance_metadata.calculated_metatdata.get_dict()
-            out['query_parameter_provenance'] = provenance_metadata.get_query_dict()
-            out['provenance_messages'] = provenance_metadata.messages
-        if include_annotations:
-            out['annotations'] = annotation_store.get_json_representation()
-    else:
-        out = particles
-
-    return json.dumps(out, indent=2)
+    json_response = JsonResponse(pd_data,
+                                 qc_stream_parameters,
+                                 provenance_metadata if include_provenance else None,
+                                 annotation_store if include_annotations else None)
+    stream_0_parameters = [p for p in stream_request.parameters if stream_keys[0].stream.id in p.streams]
+    return json_response.json(stream_keys[0], stream_0_parameters)
 
 
 def onload_netCDF(file_name):
