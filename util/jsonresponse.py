7498b4c5486207a76c596680bbfc8529342ed8b3
Stephen Zakrewsky <Stephen.Zakrewsky@raytheon.com>
2015-08-31 15:12:52 +0000

    Use xray internally for json
    Amend: pulled qc stuff into separate function
    
    Change-Id: If318e5224270a1c14096a65d54a071122d01e921

--- util/jsonresponse.py
+++ util/jsonresponse.py
@@ -1,8 +1,8 @@
 __author__ = 'Stephen Zakrewsky'
 
-from cass import store_qc_results
 from collections import OrderedDict
-from common import CachedFunction, MissingTimeException
+from common import CachedFunction
+import datamodel
 import json
 import logging
 import numpy as np
@@ -20,101 +20,37 @@ class JsonResponse(object):
         self.annotation_store = annotation_store
 
     def json(self, stream_key, parameters):
+        ds = datamodel.as_xray(stream_key, self.pd_data, self.provenance_metadata, self.annotation_store)
+        return self._json(stream_key, parameters, ds)
+
+    def _json(self, stream_key, parameters, ds):
         # setup some convenience bindings
-        primary_key = stream_key
-        pd_data = self.pd_data
-        parameters = parameters
         qc_stream_parameters = self.qc_stream_parameters
         provenance_metadata = self.provenance_metadata
         annotation_store = self.annotation_store
 
         # convert data into a list of particles
         particles = []
-
-        time_param = primary_key.stream.time_parameter
-        if time_param not in pd_data:
-            raise MissingTimeException("Time param: {} is missing from the primary stream".format(time_param))
-
-        virtual_id_sub = None
-        for index in range(len(pd_data[time_param][primary_key.as_refdes()]['data'])):
+        for index in xrange(len(ds['time'])):
             particle = OrderedDict()
-            particle_id = None
-            particle_bin = None
-
-            if not primary_key.stream.is_virtual:
-                particle['pk'] = primary_key.as_dict()
-
+            if not stream_key.stream.is_virtual:
+                particle['pk'] = stream_key.as_dict()
                 # Add non-param data to particle
-                particle['pk']['deployment'] = pd_data['deployment'][primary_key.as_refdes()]['data'][index]
-                particle['pk']['time'] = pd_data[primary_key.stream.time_parameter][primary_key.as_refdes()]['data'][index]
-                particle['provenance'] = str(pd_data['provenance'][primary_key.as_refdes()]['data'][index])
-                particle_id = pd_data['id'][primary_key.as_refdes()]['data'][index]
-                particle_bin = pd_data['bin'][primary_key.as_refdes()]['data'][index]
-            else:
-                if 'id' in pd_data:
-                    if virtual_id_sub is None:
-                        for key in pd_data['id']:
-                            if len(pd_data['id'][key]['data']) == len(pd_data[time_param][primary_key.as_refdes()]['data']):
-                                virtual_id_sub = key
-                                break
-                    particle_id = pd_data['id'][virtual_id_sub]['data'][index]
-
-                if 'bin' in pd_data:
-                    if virtual_id_sub is None:
-                        for key in pd_data['bin']:
-                            if len(pd_data['bin'][key]['data']) == len(pd_data[time_param][primary_key.as_refdes()]['data']):
-                                virtual_id_sub = key
-                                break
-                    particle_bin = pd_data['bin'][virtual_id_sub]['data'][index]
+                particle['pk']['deployment'] = ds['deployment'].values[index]
+                particle['pk']['time'] = ds['time'].values[index]
+                particle['provenance'] = str(ds['provenance'].values[index])
 
             for param in parameters:
-                if param.id in pd_data:
-                    val = None
-                    try:
-                        val = pd_data[param.id][primary_key.as_refdes()]['data'][index]
-                    except Exception as e:
-                        log.info("Failed to get data for {}: {}".format(param.id, e))
-                        continue
+                if param.name not in ds:
+                    log.info("Failed to get data for %d: Not in Dataset", (param.id,))
+                    continue
+                particle[param.name] = ds[param.name].values[index]
 
-                    if isinstance(val, np.ndarray):
-                        val = val.tolist()
-
-                    particle[param.name] = val
-
-                # add qc results to particle
-                for qc_function_name in qc_stream_parameters.get(param.name, []):
-                    qc_function_results = '%s_%s' % (param.name, qc_function_name)
-
-                    if qc_function_results in pd_data\
-                            and pd_data.get(qc_function_results, {}).get(primary_key.as_refdes(), {}).get('data') is not None:
-                        value = pd_data[qc_function_results][primary_key.as_refdes()]['data'][index]
-
-                        qc_results_key = '%s_%s' % (param.name, 'qc_results')
-                        qc_ran_key = '%s_%s' % (param.name, 'qc_executed')
-                        if qc_results_key not in particle:
-                            particle[qc_results_key] = 0b0000000000000000
-
-                        qc_results_value = particle[qc_results_key]
-                        qc_cached_function = CachedFunction.from_qc_function(qc_function_name)
-                        qc_results_mask = int(qc_cached_function.qc_flag, 2)
-
-                        particle[qc_ran_key] = qc_results_mask | particle.get(qc_ran_key, qc_results_mask)
-
-                        if value == 0:
-                            qc_results_value = ~qc_results_mask & qc_results_value
-                        elif value == 1:
-                            qc_results_value = qc_results_mask ^ qc_results_value
-
-                        particle[qc_results_key] = qc_results_value
-
-                        if particle_id is not None and particle_bin is not None:
-                            if not primary_key.stream.is_virtual:
-                                store_qc_results(qc_results_value, particle.get('pk'), particle_id, particle_bin, param.name)
-                            else:
-                                if virtual_id_sub is not None:
-                                    sub_pk = primary_key.as_dict()
-                                    sub_pk['deployment'] = pd_data['deployment'][virtual_id_sub]['data'][index]
-                                    store_qc_results(qc_results_value, sub_pk, particle_id, particle_bin, param.name)
+                qc_postfixes = ['qc_results', 'qc_executed']
+                for qc_postfix in qc_postfixes:
+                    qc_key = '%s_%s' % (param.name, qc_postfix)
+                    if qc_key in ds:
+                        particle[qc_key] = ds[qc_key].values[index]
 
             particles.append(particle)
 
@@ -131,4 +67,19 @@ class JsonResponse(object):
         else:
             out = particles
 
-        return json.dumps(out, indent=2)
+        return json.dumps(out, indent=2, cls=NumpyJSONEncoder)
+
+class NumpyJSONEncoder(json.JSONEncoder):
+    """
+    numpy array indexing will often return numpy scalars, for
+    example a = array([0.5]), type(a[0]) will be numpy.float64.
+    The problem is that numpy types are not json serializable.
+    However, they have a lot of the same methods as ndarrays, so
+    for example, tolist() can be called on a numpy scalar or
+    numpy ndarray to convert to regular python types.
+    """
+    def default(self, o):
+        if isinstance(o, (np.generic, np.ndarray)):
+            return o.tolist()
+        else:
+            return json.JSONEncoder.default(self, o)
