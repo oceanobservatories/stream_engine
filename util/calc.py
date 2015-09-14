4f0a42a032d046c1c29089d2511f9c92da2ac901
Adam Taylor <Adam.K.Taylor@raytheon.com>
2015-09-14 11:01:16 -0500

    Issue #8541 Reverted to netCDF 4 output.

--- util/calc.py
+++ util/calc.py
@@ -1301,7 +1301,7 @@ class NetCDF_Generator(object):
             os.makedirs(base_path)
         for stream_key, deployment, ds in self.stream_data.groups():
                 file_path = '%s/deployment%04d_%s.nc' % (base_path, deployment, stream_key.as_dashed_refdes())
-                ds.to_netcdf(file_path, format='NETCDF4_CLASSIC')
+                ds.to_netcdf(file_path)
                 file_paths.append(file_path)
         # build json return
         return json.dumps({'code' : 200, 'message' : file_paths }, indent=2, separators=(',',': '))
@@ -1316,7 +1316,7 @@ class NetCDF_Generator(object):
         for stream_key, deployment, ds in self.stream_data.groups():
             with tempfile.NamedTemporaryFile() as tf:
                 # interp to main times if more than one stream was in the request.
-                ds.to_netcdf(tf.name, format='NETCDF4_CLASSIC')
+                ds.to_netcdf(tf.name)
                 zf.write(tf.name, 'deployment%04d_%s.nc' % (deployment, stream_key.as_dashed_refdes(),))
 
 
