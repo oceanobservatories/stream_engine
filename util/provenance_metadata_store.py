8feb20dd4d1c995e8563569fc54052faa53e7fe1
Steven Wilderdyke <steven_wilderdyke@raytheon.com>
2016-04-15 09:19:19 -0500

    Issue #10072 Provenance is swamping science data
    Change-Id: I53a5617b12efe26be9000fc9b8cb61e7185e23c9

--- util/provenance_metadata_store.py
+++ util/provenance_metadata_store.py
@@ -1,4 +1,5 @@
 import json
+import os
 from collections import defaultdict, OrderedDict
 from multiprocessing.pool import ThreadPool
 
@@ -14,9 +15,7 @@ from util.jsonresponse import NumpyJSONEncoder
 
 metadata_threadpool = ThreadPool(10)
 
-
 log = logging.getLogger(__name__)
-
 
 class ProvenanceMetadataStore(object):
     def __init__(self, request_uuid):
@@ -61,7 +60,7 @@ class ProvenanceMetadataStore(object):
                 vals[key.as_three_part_refdes()].extend(value.get())
             return vals
         except ValueError:
-            return None
+            return {}
 
     def add_query_metadata(self, stream_request, query_uuid, query_type):
         self._query_metadata['query_type'] = query_type
@@ -76,57 +75,6 @@ class ProvenanceMetadataStore(object):
         self._query_metadata['include_annotations'] = stream_request.include_annotations
         self._query_metadata['strict_range'] = stream_request.strict_range
 
-    def add_to_dataset(self, ds, request_id):
-        if 'provenance' in ds:
-            prov_values = {}
-            prov_array = []
-            for k, v in self.get_provenance_dict().iteritems():
-                prov_values[k] = v['file_name'] + " " + v['parser_name'] + " " + v['parser_version']
-            log.debug('<%s> Building %d provenance values', request_id, len(ds.provenance.values))
-            for i in ds.provenance.values:
-                if i in prov_values:
-                    prov_array.append(prov_values[i])
-                else:
-                    prov_array.append('')
-
-            prov_array = np.array(prov_array, dtype=str)
-            ds['l0_provenance_information'] = ('obs', prov_array,
-                                               {'long_name': 'l0_provenance_data',
-                                                'description': 'file name, parser name, and parser version'})
-
-            if prov_values:
-                ds['l0_provenance_keys'] = xr.DataArray(np.array(prov_values.keys()), dims=['l0_provenance'],
-                                                        attrs={'long_name': 'l0 Provenance Keys'})
-                ds['l0_provenance_data'] = xr.DataArray(np.array(prov_values.values()), dims=['l0_provenance'],
-                                                        attrs={'long_name': 'l0 Provenance Entries'})
-
-        if self._streaming_provenance:
-            ds['streaming_provenance'] = xr.DataArray([json.dumps(self._streaming_provenance)],
-                                                      dims=['streaming_provenance_dim'],
-                                                      attrs={'long_name': 'Streaming Provenance Information'})
-
-        comp_prov = self.calculated_metadata.get_dict()
-        comp_prov = json.dumps(comp_prov, cls=NumpyJSONEncoder)
-        if comp_prov:
-            ds['computed_provenance'] = xr.DataArray([comp_prov], dims=['computed_provenance_dim'],
-                                                     attrs={'long_name': 'Computed Provenance Information'})
-
-        if self._query_metadata:
-            ds['query_parameter_provenance'] = xr.DataArray([json.dumps(self._query_metadata)],
-                                                            dims=['query_parameter_provenance_dim'],
-                                                            attrs={
-                                                                'long_name': 'Query Parameter Provenance Information'})
-
-        instrument_prov = self.get_instrument_provenance()
-        if instrument_prov:
-            ds['instrument_provenance'] = xr.DataArray([json.dumps(instrument_prov)],
-                                                       dims=['instrument_provenance_dim'],
-                                                       attrs={
-                                                           'long_name': 'Instrument Provenance Information'})
-        if self.messages:
-            ds['provenance_messages'] = xr.DataArray(self.messages,
-                                                     dims=['provenance_messages'],
-                                                     attrs={'long_name': 'Provenance Messages'})
 
     def get_json(self):
         out = OrderedDict()
@@ -139,6 +87,14 @@ class ProvenanceMetadataStore(object):
         out['requestUUID'] = self.request_uuid
         return out
 
+    def dump_json(self, filepath):
+        try:
+            if not os.path.exists(os.path.dirname(filepath)):
+                os.makedirs(os.path.dirname(filepath))
+            with open(filepath, 'a') as fh:
+                json.dump(self.get_json(), fh, indent=2, separators=(',', ': '))
+        except EnvironmentError as e:
+            log.error('Failed to write provenance file: %s', e)
 
 def _send_query_for_instrument(url):
     results = requests.get(url)
