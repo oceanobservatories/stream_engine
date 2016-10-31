273d575a99fe256886888e0d7363eda3c63479f8
Peter Cable <petercable@gmail.com>
2016-10-31 14:32:44 -0700

    Issue #11497 Incorrectly building 2-D string arrays
    Change-Id: I357c04732d926cbe53721005219fef0e4091912d

--- util/datamodel.py
+++ util/datamodel.py
@@ -207,7 +207,12 @@ def _replace_values(data_slice, value_encoding, fill_value, is_array, name):
             shapes = filter(lambda x: len(x) == max_len, shapes)
             max_shape = max(shapes)
             shp = tuple([len(unpacked)] + list(max_shape))
-            data_slice = np.empty(shp, dtype=value_encoding)
+            # temporarily encode strings as object to avoid dealing with length
+            # then cast as string below
+            if value_encoding == 'string':
+                data_slice = np.empty(shp, dtype='object')
+            else:
+                data_slice = np.empty(shp, dtype=value_encoding)
             data_slice.fill(fill_value)
             try:
                 _fix_data_arrays(data_slice, unpacked)
@@ -254,9 +259,6 @@ def _replace_values(data_slice, value_encoding, fill_value, is_array, name):
 
 def _fix_data_arrays(data, unpacked):
     if unpacked is None:
-        return
-    if len(unpacked) != data.shape[0]:
-        app.logger.warn("Mismatched dimensions could not fill array")
         return
     if len(data.shape) == 1:
         for idx, val in enumerate(unpacked):
