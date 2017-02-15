4a35fead40db98ef87529cb1eceac17808dd3c8e
Peter Cable <petercable@gmail.com>
2017-02-15 08:11:45 -0800

    Bump version number in config/default.py
    Change-Id: I41323ab4bddc80e16655f6011ea3234101f6de4e

--- config/default.py
+++ config/default.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 # General Settings         #
 ############################
 DEBUG = False
-STREAM_ENGINE_VERSION = "1.2.4"
+STREAM_ENGINE_VERSION = "1.2.5"
 # Cassandra process pool size
 POOL_SIZE = 4
 LOGGING_CONFIG = 'logging.conf'
