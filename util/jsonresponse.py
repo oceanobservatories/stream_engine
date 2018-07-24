8ae752bdc44e32ca08f68b3c91fcf0169f5a625f
mfegan <Mark_W_Fegan@Raytheon.com>
2018-07-24 13:11:40 -0500

    Issue #13299 Asyncronous download of specific parameters to CSV and NetCSF is inconsistent
    - Insure lat/lon, if available, are included in JSON format
    - Remove stream name from external parameters in CSV and JSON formats

--- util/jsonresponse.py
+++ util/jsonresponse.py
@@ -130,14 +130,18 @@ class JsonResponse(object):
             if lat_data is not None and lon_data is not None:
                 data['lat'] = lat_data
                 data['lon'] = lon_data
-                params.extend(('lat', 'lon'))
+        # always include lat/lon
+        params.extend(('lat', 'lon'))
 
-        # remaining externals
+
+        # remaining externals. rename the parameter without the stream_name prefix (12544 AC1)
         for sk in external_includes:
             for param in external_includes[sk]:
                 name = '-'.join((sk.stream_name, param.name))
                 if name in data:
-                    params.append(name)
+                    extern_data = data.pop(name)
+                    data[param.name] = extern_data
+                    params.append(param.name)
 
         if self.stream_request.include_provenance:
             params.append('provenance')
