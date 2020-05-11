818a5d61d2cec5fff0a18b1ddd851143c16faddf
tfisher <timothy.l.fisher@raytheon.com>
2020-05-11 09:51:14 -0500

    Issue 14540: Provide way to correct refdes on particle data

--- util/cass.py
+++ util/cass.py
@@ -273,7 +273,7 @@ def fetch_nth_data(stream_key, time_range, num_points=1000, location_metadata=No
     # Fetch it all if it's gonna be close to the same size
     if data_ratio < engine.app.config['UI_FULL_RETURN_RATIO']:
         log.info(
-                "CASS: Estimated points (%d) / the requested  number (%d) is less than ratio %f.  Returning all points.",
+                "CASS: Estimated points (%d) / the requested number (%d) is less than ratio %f. Returning all points.",
                 estimated_particles, num_points, engine.app.config['UI_FULL_RETURN_RATIO'])
         _, results = fetch_all_data(stream_key, time_range, location_metadata)
     # We have a small amount of bins with data so we can read them all
@@ -431,9 +431,9 @@ def fetch_all_data(stream_key, time_range, location_metadata=None):
 
 
 @log_timing(log)
-def get_full_cass_dataset(stream_key, time_range, location_metadata=None, request_id=None):
+def get_full_cass_dataset(stream_key, time_range, location_metadata=None, request_id=None, keep_exclusions=False):
     cols, rows = fetch_all_data(stream_key, time_range, location_metadata)
-    return to_xray_dataset(cols, rows, stream_key, request_id)
+    return to_xray_dataset(cols, rows, stream_key, request_id, keep_exclusions=keep_exclusions)
 
 
 @log_timing(log)
@@ -459,7 +459,7 @@ def _get_stream_row_count(stream_key, data_bin):
 
 
 @log_timing(log)
-def insert_dataset(stream_key, dataset):
+def insert_san_dataset(stream_key, dataset):
     """
     Insert an xray dataset back into CASSANDRA.
     First we check to see if there is data in the bin, if there is we either overwrite and update
@@ -468,7 +468,7 @@ def insert_dataset(stream_key, dataset):
     :param dataset: xray dataset we are updating
     :return:
     """
-    # All of the bins on SAN data will be the same in the netcdf file take the first
+    # All of the bins on SAN data will be the same in the netcdf file take the first entry
     data_bin = dataset['bin'].values[0]
     data_lists = {}
     size = dataset['index'].size
@@ -546,7 +546,8 @@ def insert_dataset(stream_key, dataset):
     # Execute query
     insert_count = 0
     fails = 0
-    for success, result in execute_concurrent_with_args(SessionManager.session(), create_rows_query, create_rows_data, concurrency=50, raise_on_first_error=False):
+    for success, result in execute_concurrent_with_args(SessionManager.session(), \
+                            create_rows_query, create_rows_data, concurrency=50, raise_on_first_error=False):
         if not success:
             fails += 1
         elif result[0][0]:
@@ -556,7 +557,8 @@ def insert_dataset(stream_key, dataset):
 
     # Update previously existing rows and new mostly empty rows
     fails = 0
-    for success, _ in execute_concurrent_with_args(SessionManager.session(), query, to_insert, concurrency=50, raise_on_first_error=False):
+    for success, _ in execute_concurrent_with_args(SessionManager.session(), \
+                        query, to_insert, concurrency=50, raise_on_first_error=False):
         if not success:
             fails += 1
     if fails > 0:
@@ -572,6 +574,308 @@ def insert_dataset(stream_key, dataset):
     metadata_service_api.index_partition_metadata_record(bin_meta)
 
     ret_val = 'Inserted {:d} and updated {:d} particles within Cassandra bin {:d} for {:s}.'.format(insert_count, update_count, data_bin, stream_key.as_refdes())
+    log.info(ret_val)
+    return ret_val
+
+
+@log_timing(log)
+def insert_dataset(stream_key, dataset):
+    """
+    Insert an xray dataset back into CASSANDRA.
+    First we check to see if there is data in the bins, if there is we
+    either overwrite and update the values or fail and notify the user
+    :param stream_key: Stream that we are using for the data insertion
+    :param dataset: xray dataset we are inserting
+    :return:
+    """
+    # capture the current data from the stream metadata table for later processing, then delete it
+    cur_stream_meta = metadata_service_api.get_stream_metadata_record(*(stream_key.as_tuple()))
+    if cur_stream_meta:
+        del_resp = metadata_service_api.delete_stream_metadata_record(*(stream_key.as_tuple()))
+        del_stts = del_resp.get('statusCode')
+        del_msg = del_resp.get('message')
+        if del_stts == "OK" and del_msg.startswith("Successfully deleted"):
+            count = cur_stream_meta.get('count')
+            first = cur_stream_meta.get('first')
+            last = cur_stream_meta.get('last')
+            log.info("Deleted stream_metadata that summarized the partition_metadata records for %s: " +
+            "count %d, first %f, last %f", stream_key.as_refdes(), count, first, last)
+        else:
+            error_msg = "Failed to delete stream_metadata for %s", stream_key.as_refdes()
+            log.error(error_msg)
+            return error_msg
+
+    # capture current partition metadata for each unique bin in the dataset for later processing
+    cur_part_meta = {}
+    for bin_val in numpy.unique(dataset['bin'].values).tolist():
+        part_meta = metadata_service_api.get_partition_metadata_record(
+            *(stream_key.as_tuple() + (bin_val, CASS_LOCATION_NAME))
+        )
+        if part_meta:
+            # capture the parts that are useful for later
+            cur_part_meta[bin_val] = (part_meta.get('count'), part_meta.get('first'), part_meta.get('last'))
+
+    # Delete the current data from the partition metadata table for the involved bins
+    for pkey, pvals in cur_part_meta.items():
+        del_resp = metadata_service_api.delete_partition_metadata_record(*(stream_key.as_tuple() +
+                                                                           (pkey, 'cass')))
+        del_stts = del_resp.get('statusCode')
+        del_msg = del_resp.get('message')
+        if del_stts == "OK" and del_msg.startswith("Successfully deleted"):
+            log.info("Deleted partition_metadata on bin %d for %s: count %d, first %f, last %f",
+                     pkey, stream_key.as_refdes(), pvals[0], pvals[1], pvals[2])
+        else:
+            error_msg = "Failed to delete partition_metadata on bin %d for %s", pkey, stream_key.as_refdes()
+            log.error(error_msg)
+            return error_msg
+
+    # get the data in the correct format
+    cols = SessionManager.get_query_columns(stream_key.stream.name)
+    dynamic_cols = cols[1:]
+    key_cols = ['subsite', 'node', 'sensor', 'bin', 'method']
+    cols = key_cols + dynamic_cols
+    arrays = {p.name for p in stream_key.stream.parameters
+              if not p.is_function and p.parameter_type == 'array<quantity>'}
+
+    # build the data lists for all the particles to be populated into cassandra
+    data_lists = {}
+    # id and provenance are expected to be UUIDs so convert them to uuids
+    data_lists['id'] = [uuid.UUID(x) for x in dataset['id'].values]
+    data_lists['provenance'] = [uuid.UUID(x) for x in dataset['provenance'].values]
+    data_lists['bin'] = [bin_val for bin_val in dataset['bin'].values]
+
+    for i in arrays:
+        data_lists[i] = [msgpack.packb(x) for x in dataset[i].values.tolist()]
+    for dc in dynamic_cols:
+        # if it is in the dataset and not already in the datalist we need to put it in the list
+        if dc in dataset and dc not in data_lists:
+            if '_FillValue' in dataset[dc].attrs:
+                temp_val = dataset[dc].values.astype(object)
+                temp_val[temp_val == dataset[dc].attrs['_FillValue']] = None
+                data_lists[dc] = temp_val
+            else:
+                data_lists[dc] = dataset[dc].values
+
+    # get the query to insert information
+    col_names = ', '.join(cols)
+    # Take subsite, node, sensor, ?, and method: the ? is for bin
+    key_str = "'{:s}', '{:s}', '{:s}', ?, '{:s}'".format(stream_key.subsite, stream_key.node, stream_key.sensor,
+                                                         stream_key.method)
+    # and the rest as ? for the dynamic columns
+    data_str = ', '.join(['?' for _ in dynamic_cols])
+    full_str = '{:s}, {:s}'.format(key_str, data_str)
+    upsert_query = 'INSERT INTO {:s} ({:s}) VALUES ({:s})'.format(stream_key.stream.name, col_names, full_str)
+    upsert_query = SessionManager.prepare(upsert_query)
+
+    count_query = "select count(1) from %s where subsite=? and node=? and sensor=? and bin=? " \
+            % (stream_key.stream.name)
+    count_query = SessionManager.prepare(count_query)
+
+    prov_query = "select * from dataset_l0_provenance where subsite=? and node=? and sensor=? and method=? " \
+            + "and deployment=?"
+    prov_query = SessionManager.prepare(prov_query)
+
+    prov_insert = "insert into dataset_l0_provenance (subsite, sensor, node, method, deployment, id, filename, " \
+            + "parsername, parserversion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    prov_insert = SessionManager.prepare(prov_insert)
+
+    # obtain the provenance data for the existing dataset
+    deployments = numpy.unique(dataset['deployment'].values).tolist()
+    prov_query_args = [(dataset.subsite, dataset.node, dataset.sensor, dataset.collection_method, deployment) \
+                       for deployment in deployments]
+    prov_query_results = execute_concurrent_with_args(SessionManager.session(), prov_query, prov_query_args)
+
+    # get the number of particles from the first dataset variable
+    first_data_var_name = dataset.data_vars.items()[0][0]
+    size = dataset[first_data_var_name].size
+
+    # create the data insertion list
+    to_insert = []
+    data_names = ['bin'] + dynamic_cols
+    for i in range(size):
+        row = [data_lists[col][i] for col in data_names]
+        to_insert.append(row)
+
+    # Build query to create rows
+    cluster_key_columns = ['subsite', 'node', 'sensor', 'bin', 'method', 'time', 'deployment', 'id']
+    create_rows_columns = ', '.join(cluster_key_columns)
+    # Fill in (subsite, node, sensor, method) leaving (bin, time, deployment, id) to be bound
+    create_rows_values = "'{:s}', '{:s}', '{:s}', ?, '{:s}', ?, ?, ?".format(
+        stream_key.subsite, stream_key.node, stream_key.sensor, stream_key.method
+    )
+    create_rows_query = 'INSERT INTO {:s} ({:s}) VALUES ({:s}) IF NOT EXISTS'.format(
+        stream_key.stream.name, create_rows_columns, create_rows_values
+    )
+    create_rows_query = SessionManager.prepare(create_rows_query)
+
+    # summarize the partition_metadata information by bin from the insertion data
+    count = prev_bin = 0
+    first = last = 0.0
+    new_part_meta = []
+    for entry in to_insert:
+        cur_bin = entry[0]
+        time = entry[1]
+        if prev_bin < 1:  # first iteration
+            prev_bin = cur_bin
+            first = last = time
+        elif prev_bin < cur_bin:
+            new_part_meta.append([prev_bin, count, first, last])
+            count = 0
+            first = last = time
+            prev_bin = cur_bin
+        else:
+            last = time
+        count += 1
+    if 0 < prev_bin:
+        new_part_meta.append([prev_bin, count, first, last])
+
+    # combine current and new metadata as needed
+    combined_part_meta = []
+    for newmeta in new_part_meta:
+        bin_val = newmeta[0]
+        count = newmeta[1]
+        first = newmeta[2]
+        last = newmeta[3]
+        curmeta = cur_part_meta.get(bin_val)
+        if curmeta:
+            curcount = curmeta[0]
+            curfirst = curmeta[1]
+            curlast = curmeta[2]
+            count += curcount
+            if curfirst < first:
+                first = curfirst
+            if curlast > last:
+                last = curlast
+        combined_part_meta.append((bin_val, count, first, last))
+
+    # Insert the combined data into the partition metadata table
+    for pmeta in combined_part_meta:
+        bin_val = pmeta[0]
+        count = pmeta[1]
+        first = pmeta[2]
+        last = pmeta[3]
+        part_meta = metadata_service_api.build_partition_metadata_record(
+            *(stream_key.as_tuple() + (bin_val, CASS_LOCATION_NAME, first, last, count)))
+        metadata_service_api.index_partition_metadata_record(part_meta)
+
+    # Combine the new data into the stream metadata table with any current metadata
+    total = sum(row[1] for row in combined_part_meta)
+    first = combined_part_meta[0][2]
+    last = combined_part_meta[len(combined_part_meta) - 1][3]
+    # adjust these if there's any current stream metadata involved
+    if cur_stream_meta:
+        curtotal = cur_stream_meta.get('count')
+        curfirst = cur_stream_meta.get('first')
+        curlast = cur_stream_meta.get('last')
+        total += curtotal
+        if curfirst < first:
+            first = curfirst
+        if curlast > last:
+            last = curlast
+
+    smeta = metadata_service_api.build_stream_metadata_record(*(stream_key.as_tuple() + (first, last, total)))
+    metadata_service_api.create_stream_metadata_record(smeta)
+
+    # Segregate insertion data list into chunks by bin
+    insert_chunks = []
+    beg = end = 0
+    for pmeta in new_part_meta:
+        bin_val = pmeta[0]
+        count = pmeta[1]
+        end += count
+        # capture the create and the upsert data into separate numpy arrays for easy access later
+        insert_chunks.append([bin_val, numpy.array(to_insert)[beg:end, :4], numpy.array(to_insert)[beg:end]])
+        beg = end
+
+    # Process each chunk of the insertion data
+    update_tracker = {}
+    for insert_chunk in insert_chunks:
+        data_bin = insert_chunk[0]
+        # We only want (bin, time, deployment, id) for first insertion to create rows
+        create_rows_data = insert_chunk[1]
+        upsert_rows_data = insert_chunk[2]
+
+        # captures insert results
+        insert_rows = {}
+        # Execute insert for each row: no individual insert is guaranteed due to
+        #  potential of Coordinator node timeout waiting for replica node response, etc
+        # tracking the insertion success isn't critical as the upsert step covers for failures
+        idx = 0
+        for success, _ in execute_concurrent_with_args(SessionManager.session(), create_rows_query, \
+                create_rows_data, concurrency=50, raise_on_first_error=False):
+            row = create_rows_data[idx]
+            insert_rows[str(row.tolist())] = 1 if success else 0
+            idx += 1
+
+        rows_inserted = sum(r[1] for r in insert_rows.items())
+        if rows_inserted < idx:
+            fails = idx - rows_inserted
+            log.info("Failed to insert %d rows within Cassandra bin %d for %s.", fails, data_bin,
+                     stream_key.as_refdes())
+            failed_rows = [r[0] for r in insert_rows.items() if r[1] == 0]
+            log.info("Rows that failed: %s", failed_rows)
+
+        # captures upsert results
+        upsert_rows = {}
+        # Update previously existing rows (if any) and newly added rows with complete set of data
+        idx = 0
+        for success, _ in execute_concurrent_with_args(SessionManager.session(), upsert_query, \
+                upsert_rows_data, concurrency=50, raise_on_first_error=False):
+            row = upsert_rows_data[idx]
+            upsert_rows[str(row.tolist())] = 1 if success else 0
+            idx += 1
+
+        rows_upserted = sum(r[1] for r in upsert_rows.items())
+        if rows_upserted < idx:
+            fails = idx - rows_upserted
+            log.warn("Failed to update %d rows within Cassandra bin %d for %s!", fails, data_bin,
+                     stream_key.as_refdes())
+            failed_rows = [r[0] for r in upsert_rows.items() if r[1] == 0]
+            log.warn("Rows that failed: %s", failed_rows)
+
+        # query the cassandra stream to capture its count for data in the stream for the stream key and bin
+        rs = SessionManager.execute(count_query, (stream_key.subsite, stream_key.node, stream_key.sensor, data_bin))
+        count = rs.current_rows[0][0]
+        if count == idx:
+            log.info("All rows were inserted into Cassandra on bin %d for %s.", data_bin, stream_key.as_refdes())
+        else:
+            fails = idx - count
+            log.warn("There are %d rows missing in Cassandra on bin %d for %s!", fails, data_bin,
+                    stream_key.as_refdes())
+
+        update_tracker[data_bin] = (idx, rows_inserted, rows_upserted, count)
+
+    # Process the provenance data, updating the refdes fields as they may have changed from the dataset values
+    prov_modified = []
+    if prov_query_results[0].success:
+        prov_query_iter = prov_query_results[0].result_or_exc
+        for row in prov_query_iter:
+            pv = ProvTuple(*row)
+            prov_modified.append(
+                pv._replace(subsite=stream_key.subsite, sensor=stream_key.sensor, node=stream_key.node))
+
+    # insert the modified provenance data
+    prov_ins_args = [(row.subsite, row.sensor, row.node, row.method, row.deployment, row.id,
+                      row.file_name, row.parser_name, row.parser_version) for row in prov_modified]
+    prov_ins_results = execute_concurrent_with_args(SessionManager.session(), prov_insert, prov_ins_args)
+    prov_insertions = list(filter(lambda r: r[0], prov_ins_results))
+    mesg = 'Provenance row insertions: {:d} of {:d} succeeded'.format(len(prov_insertions), len(prov_modified))
+    log.info(mesg)
+
+    mesg = 'update_tracker length: {:d}'.format(len(update_tracker.items()))
+    log.info(mesg)
+
+    ret_val = ""
+    for ut in update_tracker.items():
+        bin_val = ut[0]
+        counts = ut[1]
+        inserted = counts[1]
+        upserted = counts[2]
+        in_table = counts[3]
+        ret_val += '\n' if len(ret_val) > 0 else ''
+        ret_val += 'Cassandra bin {:d} for {:s}: inserted {:d}, upserted {:d}, total rows in table {:d}.'. \
+                format(bin_val, stream_key.as_refdes(), inserted, upserted, in_table)
+
     log.info(ret_val)
     return ret_val
 
@@ -611,5 +915,5 @@ def store_qc_results(qc_results_values, pk, particle_ids, particle_bins, particl
                 engine.app.config['QC_RESULTS_STORAGE_SYSTEM']))
 
 
-def initialize_worker():    # TODO: remove - unused
+def initialize_worker():
     _init()
